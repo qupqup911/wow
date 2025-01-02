4c124dd5bf0882de8202b4ce6bdf84cb7415c8bb
qupqup911 <pchen0911@gmail.com>
2025-01-03 01:52:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-let title = document.getElementById("title");
-let content = document.getElementById("content");
-let btn = document.getElementById("btn");
-let list = document.getElementById("list");
-btn.addEventListener("click", news);
-function news(){
-  list.innerHTML = list.innerHTML +`
-  <div class="news">
-    <h2>${title.value}</h2>
-    <p>${content.value}</p><hr>
-  </div>
-  `;
-}
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // 防止頁面重新加載
 
-let i=0, imgArr=new Array();
-imgArr[0] = "https://www.ncyu.edu.tw/Uploads/Icon/9a1327b4-6f82-43c1-b994-425704ae0fe8.png";
-imgArr[1] = "https://upload.wikimedia.org/wikipedia/zh/thumb/4/49/National_Chiayi_University_seal.svg/250px-National_Chiayi_University_seal.svg.png";
-imgArr[2] = "https://www.ncyu.edu.tw/Uploads/Icon/ba2db87c-8b49-43a1-b64a-30c01f76aaa5.png";
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
 
-function showImg() {
-  document.getElementById('ico').src = imgArr[i];
-  i = (i+1) % 3;
-}
-
-function show() {
-  setInterval(showImg, 2000);
-}
+    if (name && email && message) {
+        document.getElementById('responseMessage').textContent = `謝謝您的留言，${name}！我們會盡快聯繫您。`;
+    } else {
+        document.getElementById('responseMessage').textContent = '請填寫所有欄位。';
+    }
+});
